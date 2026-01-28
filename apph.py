535270a39c0c35a0fbeb22a18da91e9520021193
al222310596-eng <al222310596@gmail.com>
2026-01-27 21:43:22 -0600

    Agrega apph y controladorh usando flask, envia hugo garcia

---
+++ apph.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from controllers.controladorh import blueprint_home
+
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(blueprint_home, url_prefix='/api/v1')
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True, host='0.0.0.0')
